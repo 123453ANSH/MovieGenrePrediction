67d776a8b9c1851a4c0566e599b4d3aa65117313
Harshika Jalan <harshikajalan@berkeley.edu>
2021-05-29 00:47:54 +0530

    started building the app

---
+++ predict_page.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import numpy as np
+import pickle
+
+def load_model():
+
+data=load_model()
+
+def show_predict_page():
+    st.title("Movie Rating Prediction")
+    st.write("""" ### Enter the plot overview and draw the poster! """)
